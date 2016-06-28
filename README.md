1b33506c74816d9e0486efb1a1a7f2d033005bd0
Raul Sanchez Liebana <raul.sanchez.liebana@bbva.com>
2016-06-28 19:48:15 +0200

    Version 2.3.7

---
+++ README.md
@@ -0,0 +1,23 @@
+rancher-etcd
+==============
+
+This image is the etcd dynamic conf for rancher. It comes from [rancher-tools][rancher-tools].
+
+## Build
+
+```
+docker build -t rawmind/rancher-etcd:<version> .
+```
+
+## Versions
+
+- `2.3.7` [(Dockerfile)](https://github.com/rawmind0/rancher-etcd/blob/2.3.7/README.md)
+
+
+## Usage
+
+This image has to be run as a sidekick of [alpine-etcd][alpine-etcd], and makes available /opt/tools volume. It scans from rancher-metadata, for a etcd stack and service, and generates etcd connection string dynamicly.
+
+
+[alpine-etcd]: https://github.com/rawmind0/alpine-etcd
+[rancher-tools]: https://github.com/rawmind0/rancher-tools
---
+++ root/opt/tools/confd/bin/etcd-source.tmpl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+SERVICE_TMPL=${SERVICE_TMPL:-"/opt/tools/confd/etc/templates/etcd-source.tmpl"}
+
+cat << EOF > ${SERVICE_TMPL}
+{{\$my_ip := getv "/container/primary_ip"}}{{\$my_name := getv "/container/name"}}
+export ETCD_ADVERTISE_CLIENT_URLS=\${ETCD_ADVERTISE_CLIENT_URLS:-'http://{{\$my_ip}}:2379'}
+export ETCD_DATA_DIR=\${ETCD_DATA_DIR:-\${SERVICE_HOME}"/data"}
+export ETCD_INITIAL_ADVERTISE_PEER_URLS=\${ETCD_INITIAL_ADVERTISE_PEER_URLS:-'http://{{\$my_ip}}:2380'}
+export ETCD_INITIAL_CLUSTER=\${ETCD_INITIAL_CLUSTER:-'{{range \$i, \$containerName := ls "/service/containers"}}{{if \$i}},{{end}}{{getv (printf "/service/containers/%s/name" \$containerName)}}={{getv (printf "/service/containers/%s/primary_ip" \$containerName)}}:2380{{end}}'}
+export ETCD_INITIAL_CLUSTER_STATE=\${ETCD_INITIAL_CLUSTER_STATE:-"new"}
+export ETCD_INITIAL_CLUSTER_TOKEN=\${ETCD_INITIAL_CLUSTER_TOKEN:-'{{getv "/self/container/service_name"}}-{{getv "/self/container/stack_name"}}'}
+export ETCD_NAME=\${ETCD_NAME:-'{{\$my_name}}'}
+export ETCD_LISTEN_PEER_URLS=\${ETCD_LISTEN_PEER_URLS:-'http://{{\$my_ip}}:2380'}
+export ETCD_LISTEN_CLIENT_URLS=\${ETCD_LISTEN_CLIENT_URLS:-'http://{{\$my_ip}}:2379'}
+EOF
