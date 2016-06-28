ccf5bba338f4d707d2a9c4fdc1620507654e4705
Raul Sanchez <rawmind@gmail.com>
2016-06-28 20:36:49 +0200

    Version 2.3.7

--- root/opt/tools/confd/bin/etcd-source.tmpl.sh
+++ root/opt/tools/confd/bin/etcd-source.tmpl.sh
@@ -7,10 +7,10 @@ cat << EOF > ${SERVICE_TMPL}
 export ETCD_ADVERTISE_CLIENT_URLS=\${ETCD_ADVERTISE_CLIENT_URLS:-'http://{{\$my_ip}}:2379'}
 export ETCD_DATA_DIR=\${ETCD_DATA_DIR:-\${SERVICE_HOME}"/data"}
 export ETCD_INITIAL_ADVERTISE_PEER_URLS=\${ETCD_INITIAL_ADVERTISE_PEER_URLS:-'http://{{\$my_ip}}:2380'}
-export ETCD_INITIAL_CLUSTER=\${ETCD_INITIAL_CLUSTER:-'{{range \$i, \$containerName := ls "/service/containers"}}{{if \$i}},{{end}}{{getv (printf "/service/containers/%s/name" \$containerName)}}={{getv (printf "/service/containers/%s/primary_ip" \$containerName)}}:2380{{end}}'}
+export ETCD_INITIAL_CLUSTER=\${ETCD_INITIAL_CLUSTER:-'{{range \$i, \$containerName := ls "/service/containers"}}{{if \$i}},{{end}}{{getv (printf "/service/containers/%s/name" \$containerName)}}=http://{{getv (printf "/service/containers/%s/primary_ip" \$containerName)}}:2380{{end}}'}
 export ETCD_INITIAL_CLUSTER_STATE=\${ETCD_INITIAL_CLUSTER_STATE:-"new"}
-export ETCD_INITIAL_CLUSTER_TOKEN=\${ETCD_INITIAL_CLUSTER_TOKEN:-'{{getv "/self/container/service_name"}}-{{getv "/self/container/stack_name"}}'}
+export ETCD_INITIAL_CLUSTER_TOKEN=\${ETCD_INITIAL_CLUSTER_TOKEN:-'{{getv "/container/service_name"}}-{{getv "/container/stack_name"}}'}
 export ETCD_NAME=\${ETCD_NAME:-'{{\$my_name}}'}
 export ETCD_LISTEN_PEER_URLS=\${ETCD_LISTEN_PEER_URLS:-'http://{{\$my_ip}}:2380'}
-export ETCD_LISTEN_CLIENT_URLS=\${ETCD_LISTEN_CLIENT_URLS:-'http://{{\$my_ip}}:2379'}
+export ETCD_LISTEN_CLIENT_URLS=\${ETCD_LISTEN_CLIENT_URLS:-'http://{{\$my_ip}}:2379,http://127.0.0.1}:2379'}
 EOF
